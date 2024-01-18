d1c3cc805a2d8b46b7d5b5e500f279839172249f
Valeria <estefanyhuamani287@gmail.com>
2024-01-17 21:17:13 -0500

    create of splash

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:restobarapp/routes/route_manager.dart';
+
+import '../widgets/app_progress_indicator.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 5), () {
+      Navigator.popAndPushNamed(context, RouteManager.loginPage);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Loading Page",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.w300
+            ),),
+            AppProgressIndicator(),
+        ],),),
+    );
+  }
+}
